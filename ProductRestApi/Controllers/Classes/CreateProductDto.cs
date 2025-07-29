01345bf81493c697731a6fdb9aaac9aa4130d70d
Manuel Fas Ruiz <112648218+Fasmanuel2002@users.noreply.github.com>
2025-07-29 11:57:45 +0200

    Add files via upload

---
+++ ProductRestApi/Controllers/Classes/CreateProductDto.cs
@@ -0,0 +1,10 @@
+﻿using ProductRestApi.Classes;
+
+public class CreateProductDto
+{
+    public string ProductName { get; set; } = default!;
+    public string ProductDescription { get; set; } = default!;
+    public float ProductPrice { get; set; }
+    public float ProductDiscount { get; set; }
+    public ProductType proType { get; set; }
+}
